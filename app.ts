0ce3df45cbec4f1d9b0c880e1f0c0933475f1cf6
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-02-26 17:36:34 +1300

    add risk rating  shell controller

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { calculateCarValue } from './src/carValueController';
+import { calculateRiskRating } from './src/riskRatingController';
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,8 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 app.post('/calculate_car_value', calculateCarValue);
+app.post('/calculate_risk_rating', calculateRiskRating);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
---
+++ src/riskRatingController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+
+export function calculateRiskRating(req: Request, res: Response) {
+    
+}
+
