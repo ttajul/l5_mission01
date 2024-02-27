4164f6469d2bdff950b865e9e3478bd774debcec
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-02-28 12:02:14 +1300

    check input type for claim_history

--- src/riskRatingController.ts
+++ src/riskRatingController.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
 
 export function calculateRiskRating(req: Request, res: Response) {
-    
+    const {claim_history} = req.body;
+
+    //check if claim_history is provided in the request body
+    if (!claim_history || typeof claim_history !== 'string') {
+        return res.status(400).json({error: 'Invalid input. Claim history must be provided as string'
+    });
+    }
 }
 
