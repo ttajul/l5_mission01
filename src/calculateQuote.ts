495cecb6b1de9554b8d36b1b7b8f56ee1e333e1f
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-02-28 14:03:24 +1300

    check if car value and risk rating are valid number

--- src/calculateQuote.ts
+++ src/calculateQuote.ts
@@ -7,6 +7,10 @@ export function calculateQuote(req: Request, res: Response) {
         // extract car_value and  risk_rating from request body
         const {car_value, risk_rating} = req.body;
         
+        // check if car_value and risk_rating are valid numbers
+        if (!car_value || isNaN(car_value) || !risk_rating || isNaN(risk_rating)) {
+            return res.status(400).json({error: 'Invalid input. car value and risk rating must be number'})
+        }
 
     } catch (error) {
         res.status(500).json({error: 'An error occur when processing the request'})
